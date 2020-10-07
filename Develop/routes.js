ff415e967599ebeceb23b0296ec79959a53c6569
Eddie Villarreal <eddie.villarreal@outlook.com>
2020-10-07 17:53:53 -0500

    added start heroku

--- Develop/routes.js
+++ Develop/routes.js
@@ -31,4 +31,29 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "public/index.html"));
     });
 
+    app.post("/api/notes", (req, res) => {
+        let newNote = req.body;
+    
+        readFileAsync("./db/db.json", "utf8")
+            .then((result, err) => {
+                if (err) console.log(err);
+                return Promise.resolve(JSON.parse(result));
+            })
+            .then(data => {
+                newNote.id = getLastIndex(data) + 1;
+                (data.length > 0) ? data.push(newNote): data = [newNote];
+                return Promise.resolve(data);
+            })
+            .then(data => {
+                writeFileAsync("./db/db.json", JSON.stringify(data));
+                res.json(newNote);
+            })
+            .catch(err => {
+                if (err) throw err;
+            });
+    });
+
+    
+
+
 };
--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-
+//pure-cliffs-1976o
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require("./routes.js");
 
+app.use(express.static("public"));
 
 
 app.listen(PORT, function() {
